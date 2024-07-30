60c2a69f6258420575e54a1929c735354f220238
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2024-07-30 20:02:56 +0600

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+// imports
 const fs = require('fs');
 const path = require('path');
 const gulp = require('gulp');
@@ -16,66 +17,89 @@ const Yaml = require('js-yaml-import');
 const options = minimist(process.argv.slice(2));
 const server = browserSync.create();
 
-const getDirectories = (source) => {
-  return fs
+// getDirectories
+const getDirectories = (source) =>
+  fs
     .readdirSync(source, { withFileTypes: true })
     .filter((dirent) => dirent.isDirectory())
     .map((dirent) => dirent.name);
-};
 
-function transpile(cb) {
-  return gulp
-    .src(path.join(__dirname, './src/templates/**/*/registry.mjml'))
-    .pipe(
-      posthtml([
-        require('posthtml-include')({ root: path.join(__dirname, `./src`) }),
-        require('posthtml-extend')({ root: path.join(__dirname, `./src`) }),
-      ]),
-    )
-    .pipe(
-      data((file) => {
-        const yaml = new Yaml([
-          path.join(__dirname, 'src'),
-          ...getDirectories(path.join(__dirname, 'src')).map((dir) =>
-            path.join(__dirname, `./src/${dir}`),
-          ),
-        ]);
-        const dirpath = path.dirname(file.path).replace(__dirname, '');
-        const yamlFile = yaml.read(path.join(__dirname, dirpath, 'data.yaml'));
-        return yamlFile;
-      }),
-    )
-    .pipe(
-      pipeIf(
-        options.dev,
-        nunjucks.compile(
-          {
-            startMsoConditionalTag: '<!--[if mso | IE]><!-- -->',
-            startMsoNegationConditionalTag: '<!--[if !mso]><!-- -->',
-            endConditionalTag: '<!--<![endif]-->',
-            startHideConditionalTag: '<!--[if mso | IE]>',
-            endHideConditionalTag: '<![endif]-->',
-          },
-          {
-            env: new nunjucksEngine.Environment(
-              new nunjucksEngine.FileSystemLoader([
-                path.join(__dirname, 'src'),
-                ...getDirectories(path.join(__dirname, 'src')).map((dir) =>
-                  path.join(__dirname, `./src/${dir}`),
-                ),
-              ]),
-              { autoescape: false },
+function transpile(_cb) {
+  return (
+    gulp
+      // single template - high perfomance
+      // .src(path.join(__dirname, './src/templates/**/*/registry.mjml'))
+
+      // all templates - low perfomance
+      .src(path.join(__dirname, './src/templates/**/*.mjml'))
+
+      // post html
+      .pipe(
+        posthtml([
+          require('posthtml-include')({ root: path.join(__dirname, `./src`) }),
+          require('posthtml-extend')({ root: path.join(__dirname, `./src`) }),
+        ])
+      )
+
+      // data yml file
+      .pipe(
+        data((file) => {
+          const yaml = new Yaml([
+            path.join(__dirname, 'src'),
+            ...getDirectories(path.join(__dirname, 'src')).map((dir) =>
+              path.join(__dirname, `./src/${dir}`)
             ),
-          },
-        ),
-      ),
-    )
-    .pipe(rename((path) => ({ dirname: '', basename: path.dirname, extname: '.mjml' })))
-    .pipe(gulp.dest(path.join(__dirname, './dist/mjml')))
-    .pipe(mjml(mjmlEngine, { beautify: options.dev, minify: options.prod }))
-    .pipe(gulp.dest(path.join(__dirname, './dist/html')));
+          ]);
+
+          const dirpath = path.dirname(file.path).replace(__dirname, '');
+          const yamlFile = yaml.read(path.join(__dirname, dirpath, 'data.yaml'));
+
+          return yamlFile;
+        })
+      )
+
+      // add html comments
+      .pipe(
+        pipeIf(
+          options.dev,
+          nunjucks.compile(
+            {
+              startMsoConditionalTag: '<!--[if mso | IE]><!-- -->',
+              startMsoNegationConditionalTag: '<!--[if !mso]><!-- -->',
+              endConditionalTag: '<!--<![endif]-->',
+              startHideConditionalTag: '<!--[if mso | IE]>',
+              endHideConditionalTag: '<![endif]-->',
+            },
+            {
+              env: new nunjucksEngine.Environment(
+                new nunjucksEngine.FileSystemLoader([
+                  path.join(__dirname, 'src'),
+                  ...getDirectories(path.join(__dirname, 'src')).map((dir) =>
+                    path.join(__dirname, `./src/${dir}`)
+                  ),
+                ]),
+                { autoescape: false }
+              ),
+            }
+          )
+        )
+      )
+
+      // rename
+      .pipe(rename((path) => ({ dirname: '', basename: path.dirname, extname: '.mjml' })))
+
+      // mjml destenation
+      .pipe(gulp.dest(path.join(__dirname, './dist/mjml')))
+
+      // minify
+      .pipe(mjml(mjmlEngine, { beautify: options.dev, minify: options.prod }))
+
+      // html destenation
+      .pipe(gulp.dest(path.join(__dirname, './dist/html')))
+  );
 }
 
+// gulp watch
 function watch(cb) {
   if (!options.build) {
     gulp.watch(['./src/**/*.mjml', './src/**/*.yaml'], gulp.series(transpile, reload));
@@ -84,22 +108,25 @@ function watch(cb) {
   }
 }
 
+// reload
 function reload(done) {
   server.reload();
   done();
 }
 
+// gulp dev
 function serve(done) {
   server.init({ server: { baseDir: './dist/html', directory: true } });
   done();
 }
 
-function move(cb) {
-  return gulp
-    .src(`./dist/html/**/*`)
-    .pipe(gulp.dest(path.join(__dirname, options.to)));
+// gulp move
+function move(_cb) {
+  return gulp.src(`./dist/html/**/*`).pipe(gulp.dest(path.join(__dirname, options.to)));
 }
 
 exports.serve = gulp.parallel(gulp.series(transpile, serve), watch);
+
 exports.build = transpile;
+
 exports.move = move;
